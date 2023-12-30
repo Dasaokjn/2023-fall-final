330efbd6f12ab4e098526bd707608ea6bb34f235
Dasaokjn <131093951+Dasaokjn@users.noreply.github.com>
2023-12-30 10:47:51 +0900

    Create ex1.js

---
+++ ex1.js
@@ -0,0 +1,9 @@
+function imgchange1() {
+    var image = document.getElementById("logo");
+    image.src = '.jpg';
+}
+
+function imgchange2() {
+    var image = document.getElementById("logo");
+    image.src = '.jpg';
+}
